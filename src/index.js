ebdee14719b02965c090406d032e0fcd715df1b4
tasarma <tasarma@itu.edu.tr>
2022-11-08 17:26:56 +0100

    adding a global authorization

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 import cors from 'cors';
 import express from 'express';
-import { ApolloServer } from 'apollo-server-express';
+import { ApolloServer, AuthenticationError } from 'apollo-server-express';
 
 
 import schema from './schema'
@@ -13,6 +13,17 @@ const app = express();
 const eraseDatabaseOnSync = true;
 
 app.use(cors());
+
+const getMe = async req => {
+  const token = req.headers['x-token'];
+  if(token) {
+    try {
+      return await jwt.verify(token, process.env.SECRET);
+    } catch (error) {
+      throw new AuthenticationError('Your session expired. Sign in again.');
+    }
+  }
+};
 
 async function startApolloServer() {
   const server = new ApolloServer({
@@ -31,11 +42,15 @@ async function startApolloServer() {
     },
 
     // bir kere db yi gecirmeye calis her seferinde yenilemesin
-    context: async () => ({
-      models,
-      me: await models.User.findByLogin('rwieruch'),
-      secret: process.env.SECRET,
-    }),
+    context: async ({ req }) => {
+      const me = await getMe(req);
+      
+      return { 
+        models,
+        me,
+        secret: process.env.SECRET,
+      };
+    },
 
     // introspection: true,
   });
